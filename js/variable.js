3f7f9058dda0be506dce2b2fe4133b6e1f8644b3
Alix <alix.petit@lepoles.org>
2021-12-16 15:20:53 +0100

    jc Javascript

---
+++ js/variable.js
@@ -0,0 +1,10 @@
+"use strict";
+//alert("test"); comantaire1
+/**
+ * commentaire 2
+ * dzad
+ */
+// kamelCase on commence en minuscule et a chaque nouveaux mots on met un majuscule
+//une variable c'est un espace dans lequel on stock des choses c'et une boiteque l'on nomme
+var unTexte = "voici un texte"; //ce qui est à  droite donne sa valeur a ce qui est à gauche/
+console.log(unTexte);
